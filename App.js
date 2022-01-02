3cb245fa42c1cd9b32f3791bc226ffc37e3572f7
Ömer Can <67295748+omercandemir@users.noreply.github.com>
2022-01-02 21:57:04 +0300

    header area compete

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View, Image } from 'react-native';
+import { styles } from "./styles/style";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <SafeAreaView style={styles.header}>
+        <View style={{ flex: 0.6, paddingTop: 20 }}>
+          <Text style={styles.headerText}>Find Your Next Trip!</Text>
+        </View>
+        <View style={{ flex: 0.35, alignItems: 'flex-end', paddingTop: 20 }}>
+          <Image source={require("./assets/images/default.jpg")} style={styles.headerImage}></Image>
+        </View>
+      </SafeAreaView>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ styles/style.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native';
+export const styles = StyleSheet.create({
+    header: {
+        flexDirection: "row"
+    },
+    headerText: {
+        fontSize: 45,
+        fontWeight: 'bold',
+        paddingLeft: 20,
+    },
+    headerImage: {
+        width: 60,
+        height: 60,
+        borderRadius: 50,
+        alignItems: "flex-end"
+    },
+    p20: {
+        paddingTop: 20
+    }
+});
